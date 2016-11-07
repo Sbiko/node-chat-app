095bb1b8bfc3283e46a0183c9db22c4a692601ed
youness <sbikowak@gmail.com>
2016-11-07 22:04:14 +0000

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -36,12 +36,13 @@ socket.on('newLocationMessage', function (message) {
 
 $(document).ready(function(){
   $('#message-form').on('submit', function(e) {
+    var messageTextbox = jQuery('[name=message]');
     e.preventDefault();
     socket.emit('createMessage', {
       from: 'User',
-      text: jQuery('[name=message]').val()
+      text: messageTextbox.val()
     }, function() {
-
+      messageTextbox.val('');
     });
   });
 
@@ -51,12 +52,16 @@ locationButton.on('click', function () {
   if (!navigator.geolocation) {
     return alert('geolcation not supported with your browser')
   }
+  locationButton.attr('disabled','disabled').text('Sendfing location ...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location.')
   });
 });
