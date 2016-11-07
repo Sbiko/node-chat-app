3ee599f139f6ef7a81f01fb1e06f0e1e25d20ea9
youness <sbikowak@gmail.com>
2016-11-07 15:56:57 +0000

    Add form for messages and show incoming messages in bbrowser

--- public/js/index.js
+++ public/js/index.js
@@ -12,4 +12,28 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('New message recived', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+// socket.emit('createMessage', {
+//   from: 'Frank',
+//   text: 'hi'
+// }, function (data) {
+//   console.log('Gotta it', data);
+// });
+
+$(document).ready(function(){
+  $('#message-form').on('submit', function(e) {
+    e.preventDefault();
+    socket.emit('createMessage', {
+      from: 'User',
+      text: jQuery('[name=message]').val()
+    }, function() {
+
+    });
+  });
+
 });
