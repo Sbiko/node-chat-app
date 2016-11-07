ad993ea23b80621f1070050c59e4d50b94c90589
youness <sbikowak@gmail.com>
2016-11-07 00:45:13 +0000

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -15,14 +15,30 @@ io.on('connection', (socket) => {
   console.log('New user connected');
 
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the app',
+    createdAt: new Date().getTime()
+  });
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'new User joined',
+
+  });
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    })
+    // io.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // })
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+
   });
   socket.on('disconnect', () => {
     console.log('User was disconnected');
