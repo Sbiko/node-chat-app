0fcd97eea0dfd693f5c18ee4238af4fbf78edef9
youness <sbikowak@gmail.com>
2016-11-07 00:18:20 +0000

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-  socket.emit('createMessage', {
-    to: 'khonaflah',
-    text: 'wesh wesh l3achra'
-  });
 
 });
 
--- server/server.js
+++ server/server.js
@@ -15,13 +15,14 @@ io.on('connection', (socket) => {
   console.log('New user connected');
 
 
-  socket.emit('newMessage', {
-    from: 'youness',
-    text: 'fine a sat',
-    createdAt: 123
-  });
-  socket.on('createMessage', (newMessage) => {
-    console.log('createMessage', newMessage);
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
   });
   socket.on('disconnect', () => {
     console.log('User was disconnected');
