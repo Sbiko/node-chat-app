99873b9ac5d209376e5244995a6d5218d7ce9df1
youness <sbikowak@gmail.com>
2016-11-08 00:30:47 +0000

    Add mustache.js for message tempaltes

--- public/js/index.js
+++ public/js/index.js
@@ -12,20 +12,25 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
-  jQuery('#messages').append(li);
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+  jQuery('#messages').append(html);
 });
 socket.on('newLocationMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  var  a  = jQuery('<a target="_blank">My current location<a/>');
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  })
 
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
 });
 
 // socket.emit('createMessage', {
@@ -53,7 +58,7 @@ locationButton.on('click', function () {
   if (!navigator.geolocation) {
     return alert('geolcation not supported with your browser')
   }
-  locationButton.attr('disabled','disabled').text('Sendfing location ...');
+  locationButton.attr('disabled','disabled').text('Sending location ...');
 
   navigator.geolocation.getCurrentPosition(function (position) {
     locationButton.removeAttr('disabled').text('Send location');
